20c462cc2bc2740caf9a8d97c2116ab98d63085c
a <a>
2025-12-10 22:32:59 +0500

    first commit

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "../pages/Home";
+import MainLoyaut from "../loyauts/MainLoyaut";
+
+function Routes() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLoyaut />,
+      children: [
+        {
+          index: true,
+          element: <Home/>,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
+}
+
+export default Routes;
